1acfadae579c9a682d89e8a956d340567e0cdf3a
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2021-04-14 22:53:59 +0300

    Updated file naming

---
+++ presentations_downloader.py
@@ -0,0 +1,85 @@
+import json
+import re
+import sys
+import time
+
+import requests
+from googlesearch import search
+from fake_headers import Headers
+
+from util import load_config, open_json_file_or_create_and_dump_obj
+
+config = load_config()
+presentations_cache = open_json_file_or_create_and_dump_obj(config["presentationsCacheFilePath"], [])
+
+
+# TODO scrape from slideshare
+# TODO scrape from Bing
+
+def scrape_presentations_to_dir(search_keywords=config["searchKeywords"],
+                                presentations_dir_path=config["downloadDirectory"],
+                                presentations_cache_file_path=config[
+                                    "presentationsCacheFilePath"]):
+    urls = __scrape_presentation_urls__(search_keywords)
+    paths_to_presentations = []
+    for url in urls:
+        try:
+            response = requests.get(url, headers=Headers(headers=True).generate())
+        except:
+            e = sys.exc_info()[0]
+            print(f"Failed to download {url}. Error is:\n{e}")
+            continue
+        # TODO deal with duplicate file names overwriting each other
+        presentation_file_name = __get_file_name_from_response__(response)
+        presentation_file_path = f"{presentations_dir_path}/{presentation_file_name}"
+
+        with open(presentation_file_path, 'wb') as presentation_file:
+            presentation_file.write(response.content)
+        print(f"Downloaded {url} to {presentation_file_path}.")
+        __update_presentations_cache__(url, presentation_file_path, presentations_cache_file_path)
+        paths_to_presentations.append(presentation_file_path)
+    return paths_to_presentations
+
+
+def __update_presentations_cache__(url, presentation_file_path, presentations_cache_file_path):
+    presentations_cache.append({"path": presentation_file_path, "url": url})
+    with open(presentations_cache_file_path, 'w') as f:
+        json.dump(presentations_cache, f)
+
+
+# TODO ensure file name ends with pptx
+def __get_file_name_from_response__(response):
+    """
+    Get filename from content-disposition
+    """
+    content_disposition = response.headers.get('content-disposition')
+    if content_disposition:
+        file_name_from_content_disposition = re.findall('filename=(.+)', content_disposition)
+        if len(file_name_from_content_disposition) > 0:
+            return file_name_from_content_disposition[0].strip('"')
+    return __extract_file_name_from_url__(response.url).strip('"')
+
+
+def __extract_file_name_from_url__(url):
+    url_regex_file_name = re.findall(r"(?=\w+\.\w{3,4}$).+", url)
+    if url_regex_file_name:
+        return url_regex_file_name[0]
+    else:
+        return url
+
+
+def __scrape_presentation_urls__(search_keywords, sleep_secs=30):
+    presentations_urls = []
+    cached_urls = [presentation["url"] for presentation in presentations_cache]
+
+    for search_keyword in search_keywords:
+        search_query = f"{search_keyword} filetype:pptx"
+        print(f"Searching for '{search_query}'")
+        results = search(search_query, num_results=100)  # 100 is max
+        new_results = [new_result for new_result in results if
+                       new_result not in presentations_urls and new_result not in cached_urls]
+        presentations_urls += new_results
+        print(f"Will sleep for {sleep_secs} to avoid rate limit")
+        time.sleep(sleep_secs)
+
+    return presentations_urls
--- presentations_text_extractor.py
+++ presentations_text_extractor.py
@@ -3,14 +3,26 @@ import os
 
 from pptx import Presentation
 
-from presentations_scraper import scrape_presentations_to_dir
 from util import calculate_length_stats_for_list_of_strings, load_config
+
+# TODO write documentation for functions
+
 
 config = load_config()
 
 
-# TODO write documentation for functions
-def load_presentations_objects_from_dir(dir_path):
+def extract_presentations_texts(dir_path=config["downloadDirectory"], flattened=False,
+                                result_json_file_path=""):
+    presentations = __load_presentations_objects_from_dir__(dir_path)
+    presentations = [__extract_presentation_text__(presentation, flattened) for presentation in
+                     presentations]
+    if result_json_file_path != "":
+        with open(result_json_file_path, 'w') as f:
+            json.dump(presentations, f, ensure_ascii=False)
+    return presentations
+
+
+def __load_presentations_objects_from_dir__(dir_path):
     presentations = []
     for root, dirs, files in os.walk(dir_path):
         for name in files:
@@ -23,7 +35,7 @@ def load_presentations_objects_from_dir(dir_path):
     return presentations
 
 
-def extract_presentation_text(presentation, is_flattened):
+def __extract_presentation_text__(presentation, is_flattened):
     # Collect notes
     slides_notes_texts = [
         slide.notes_slide.notes_text_frame.text if slide.has_notes_slide and slide.notes_slide.notes_text_frame is not None
@@ -46,25 +58,3 @@ def extract_presentation_text(presentation, is_flattened):
                                      zip(slides_notes_texts, slides_body_texts)],
                 "bodyTextLengthStats": calculate_length_stats_for_list_of_strings(slides_body_texts),
                 "noteTextLengthStats": calculate_length_stats_for_list_of_strings(slides_notes_texts)}
-
-
-def extract_presentations_texts(dir_path=config["downloadDirectory"], flattened=False,
-                                result_json_file_path=""):
-    presentations = load_presentations_objects_from_dir(dir_path)
-    presentations = [extract_presentation_text(presentation, flattened) for presentation in
-                     presentations]
-    if result_json_file_path != "":
-        with open(result_json_file_path, 'w') as f:
-            json.dump(presentations, f, ensure_ascii=False)
-    return presentations
-
-
-def main():
-    # presentation_urls = scrape_presentation_urls()
-    # presentation_urls = ["https://enable.unc.edu/wp-content/uploads/2019/05/CHIP_XRHealthcare.pptx"]
-
-    print(extract_presentations_texts(result_json_file_path="parsing_presentations_result.json"))
-
-
-if __name__ == '__main__':
-    main()

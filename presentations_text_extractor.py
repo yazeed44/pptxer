354022e838f9bae0eb1560a5a0f6bd537996ae56
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2021-04-16 17:17:11 +0300

    Now there is only one public function of this file, and for its inputs, it only takes path(s) to pptx files or directories that contains pptx files

--- presentations_text_extractor.py
+++ presentations_text_extractor.py
@@ -11,28 +11,57 @@ from util import calculate_length_stats_for_list_of_strings, load_config
 config = load_config()
 
 
-def extract_presentations_texts(dir_path=config["downloadDirectory"], flattened=False,
-                                result_json_file_path=""):
-    presentations = __load_presentations_objects_from_dir__(dir_path)
-    presentations = [__extract_presentation_text__(presentation, flattened) for presentation in
-                     presentations]
+def extract_presentations_texts(paths, combine_results_of_different_paths=True,
+                                presentation_slide_texts_flattened=False, result_json_file_path=""):
+    if isinstance(paths, str):
+        presentations = __extract_presentation_texts_from_path__(paths, presentation_slide_texts_flattened)
+    else:
+        presentations = [__extract_presentation_texts_from_path__(path, presentation_slide_texts_flattened) for path in
+                         paths]
+
+        presentations = sum(presentations, []) if combine_results_of_different_paths else presentations
+
     if result_json_file_path != "":
         with open(result_json_file_path, 'w') as f:
             json.dump(presentations, f, ensure_ascii=False)
+
+    return presentations
+
+
+def __extract_presentation_texts_from_path__(path, flattened, ):
+    # If path is to a directory, then navigate that directory and extract text from any pptx file within it
+    # If path is to a single file, then extract text from that specific pptx file
+    if os.path.isdir(path):
+        presentations = __load_presentations_objects_from_dir__(path)
+    else:
+        presentations = __load_presentations_objects_from_file_paths__(path)
+    presentations = [__extract_presentation_text__(presentation, flattened) for presentation in
+                     presentations]
+
+    return presentations
+
+
+def __load_presentations_objects_from_file_paths__(file_paths):
+    if isinstance(file_paths, str):
+        file_paths = [file_paths]
+    presentations = []
+    for path in file_paths:
+        try:
+            presentations.append({"path": path, "presentationObj": Presentation(path)})
+        except:
+            print(
+                f"Unable to process {path} . It is likely to be corrupted or incomplete. Please ensure that the input is a valid pptx file")
     return presentations
 
 
 def __load_presentations_objects_from_dir__(dir_path):
-    presentations = []
+    paths = []
     for root, dirs, files in os.walk(dir_path):
         for name in files:
             if name.endswith("pptx"):
-                path = os.path.join(root, name)
-                try:
-                    presentations.append({"path": path, "presentationObj": Presentation(path)})
-                except:
-                    print(f"Unable to process {path} . It is likely to be corrupted or incomplete")
-    return presentations
+                paths.append(os.path.join(root, name))
+
+    return __load_presentations_objects_from_file_paths__(paths)
 
 
 def __extract_presentation_text__(presentation, is_flattened):

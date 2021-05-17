97b9071f0e064440aad713a61d65605de832c2f1
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2021-05-17 19:30:08 +0300

    only export public functions

--- presentations_scraper/__init__.py
+++ presentations_scraper/__init__.py
@@ -1,6 +1,4 @@
-"""Scrape presentations and extract their textx"""
-from presentations_downloader import *
-from presentations_text_extractor import *
-from util import *
-
-__version__ = '0.1.0'
+"""Scrape presentations and extract their text"""
+from presentations_downloader import scrape_presentations_to_dir
+from presentations_text_extractor import extract_presentations_texts
+__version__ = "0.1.0"
